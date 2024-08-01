dfac3e734de8545c78f2522f1bb343d500fada32
ZaksenCode <maksimsidorkin2006@mail.ru>
2024-08-01 14:16:55 +0300

    Update

--- src/main/kotlin/me/zaksen/plugin/Plugin.kt
+++ src/main/kotlin/me/zaksen/plugin/Plugin.kt
@@ -1,10 +1,11 @@
-package me.zaksen
+package me.zaksen.plugin
 
 import org.bukkit.plugin.java.JavaPlugin
 
 class Plugin: JavaPlugin() {
-    override fun onEnable() {
 
+    override fun onEnable() {
+        println("Hello from Plugin!")
     }
 
     override fun onDisable() {
